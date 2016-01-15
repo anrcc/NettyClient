bc06bee7bebeaca7678fe118cabd715abd62ee99
Banson <banson@chatlink.cn>
2016-01-15 12:02:59 +0800

    发送小小

--- src/cc/anr/handlers/TimeClientHandler.java
+++ src/cc/anr/handlers/TimeClientHandler.java
@@ -1,27 +1,29 @@
 package cc.anr.handlers;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TimeClientHandler extends ChannelHandlerAdapter {
 
-	private ByteBuf clientMessage;
 	ChannelHandlerContext ctx;
-	public TimeClientHandler() {
-
-		byte[] req = "Call-User-Service".getBytes();
-		clientMessage = Unpooled.buffer(req.length);
-		clientMessage.writeBytes(req);
+	public TimeClientHandler() {		
+		
+		//定时给服务器发消息 10 秒
+		Timer timer = new Timer();
+		long delay = 0;
+		long intevalPeriod = 10 * 1000;
+		// schedules the task to be run in an interval
+		timer.scheduleAtFixedRate(task, delay, intevalPeriod);
+		
 	}
 
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
 		this.ctx=ctx;
-		System.out.println("====TimeClientHandler===channelActive=======");
-		sendClientMsg();
-//		new Send().run();
 
 	}
 
@@ -29,8 +31,9 @@ public class TimeClientHandler extends ChannelHandlerAdapter {
 	public void channelRead(ChannelHandlerContext ctx, Object msg) {
 
 		ByteBuf m = (ByteBuf) msg;
-		String content = m.toString(io.netty.util.CharsetUtil.UTF_8);
-		System.out.println(content);
+		String message = m.toString(io.netty.util.CharsetUtil.UTF_8);
+		System.out.println("Netty-Client:Receive Message:" + message);
+
 		m.release();
 
 	}
@@ -38,7 +41,6 @@ public class TimeClientHandler extends ChannelHandlerAdapter {
 	
 	@Override
 	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-		sendClientMsg();
 		super.channelReadComplete(ctx);
 	}
 	
@@ -49,28 +51,22 @@ public class TimeClientHandler extends ChannelHandlerAdapter {
 	}
 	
 	
-	
-	public void sendClientMsg() {
+	public void sendClientMsg(ChannelHandlerContext ctx) {
 		if (ctx == null || !ctx.channel().isActive()) {
 			return;
 		}
-		ctx.writeAndFlush(clientMessage);
+		String msg="A voice from afar:"+ctx.channel().id().asShortText();
+		byte[] sdf =msg.getBytes();
+		ByteBuf contentBuf = ctx.alloc().buffer(sdf.length);
+		contentBuf.writeBytes(sdf);
+		ctx.writeAndFlush(contentBuf);
 	}
 
-	class Send implements Runnable {
-
-		@Override
-		public void run() {
-			while (true) {
-				sendClientMsg();
-				try {
-					Thread.sleep(10000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-
-		}
-	}
+	
+	  TimerTask task = new TimerTask() {  
+          @Override  
+          public void run() {  
+        		  sendClientMsg(ctx);        	  
+          }  
+      };
 }
